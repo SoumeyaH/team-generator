4562149603f4706921061932a863e84b4682f652
Soumeya <soumeyahassan47@gmail.com>
2021-05-16 14:31:26 +0100

    engineer works

--- src/tests/engineer.test.js
+++ src/tests/engineer.test.js
@@ -1,43 +1,35 @@
 const Engineer = require("../lib/engineer");
 
 describe("Engineer", () => {
+  const engineerAnswers = {
+    _name: "r",
+    _id: "3",
+    _email: "g@g.com",
+    _github: "g",
+  };
+  // put in mock answers
   describe("constructor tests", () => {
     test("should construct engineer object with with name, id, email, github account and role", () => {
-      const engineer = new Engineer(
-        "Sam",
-        222,
-        "sam22@gmail.com",
-        "https://github.com/samken22"
-      );
+      const engineer = new Engineer(engineerAnswers);
       expect(engineer).toEqual({
         role: "Engineer",
-        name: "Sam",
-        id: 222,
-        email: "sam22@gmail.com",
-        github: "https://github.com/samken22",
+        name: "r",
+        id: "3",
+        email: "g@g.com",
+        github: "g",
       });
     });
   });
   describe("method tests", () => {
     test("should return role when getRole is called", () => {
-      const engineer = new Engineer(
-        "Sam",
-        222,
-        "sam22@gmail.com",
-        "https://github.com/samken22"
-      );
+      const engineer = new Engineer(engineerAnswers);
 
       expect(engineer.getRole()).toEqual("Engineer");
     });
     test("should return github when getGithub is called", () => {
-      const engineer = new Engineer(
-        "Sam",
-        222,
-        "sam22@gmail.com",
-        "https://github.com/samken22"
-      );
+      const engineer = new Engineer(engineerAnswers);
 
-      expect(engineer.getGithub()).toEqual("https://github.com/samken22");
+      expect(engineer.getGithub()).toEqual("g");
     });
   });
 });
